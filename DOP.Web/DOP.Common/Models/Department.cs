c6ea5054b4b2ed81d5b700638b9ac4d00a3c6390
Валерий Пинязик <arclonerholdon@gmail.com>
2017-12-02 15:26:13 +0300

    DOP [2] Added models in DOP.Common
    Added models Department.cs and Personnel.cs

---
+++ DOP.Web/DOP.Common/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOP.Common.Models
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Details { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ DOP.Web/DOP.Common/Models/Personnel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOP.Common.Models
+{
+    public class Personnel
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Adress { get; set; }
+        public bool FamilyState { get; set; }
+        public string Photo { get; set; }
+        public int PersonnelId { get; set; }
+        public int DepartmentId { get; set; }
+        public int ContactId { get; set; }
+    }
+}
